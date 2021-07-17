96997de2f74d6080f4a26fb1cd824778eddd4e90
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-07-17 10:09:23 -0400

    added linter and fixed a css path

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SESS_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+	secret: process.env.SESS_SECRET,
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
 };
 
 app.use(session(sess));
@@ -33,10 +33,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+	app.listen(PORT, () => console.log('Now listening'));
 });
