75930a0af16e2b33a87f5eb4c06e255c7eae6a80
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-19 16:13:04 -0400

    pulling second movie quote.

--- public/javascript/movieqoutes.js
+++
@@ -1,8 +0,0 @@
-// const movieQuote = require("popular-movie-quotes");
-
-// function movieQuotes() {
-//     console.log(movieQuote.getAll());
-// }
-
-// document 
-// .querySelector('.login-form').addEventListener('submit', movieQuotes)
